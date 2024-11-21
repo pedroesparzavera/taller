0e1610fdb08cfd99016ba2cd7957958dfd48b894
pedroesparzavera <pedroesparzavera@gmail.com>
2024-11-21 09:08:07 -0300

    Primer commit

---
+++ base_app/forms.py
@@ -0,0 +1,153 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, SetPasswordForm
+from django.contrib.auth.models import User
+from base_app.models import Perfil
+
+from base_app.utils import get_marcas, get_condiciones, get_categorias
+
+
+class PublicarProductoForm(forms.Form):
+    nombre = forms.CharField(label="Nombre", max_length=50)
+    descripcion = forms.CharField(
+        label="Descripción",
+        max_length=500,
+        widget=forms.Textarea
+    )
+    precio = forms.DecimalField(
+        label="Precio",
+        max_digits=10,
+        decimal_places=2
+    )
+    descuento = forms.DecimalField(
+        label="Descuento",
+        max_digits=5,
+        decimal_places=2,
+        required=False
+    )
+    imagenes = forms.FileField(
+        label="Imágenes",
+        widget=forms.FileInput(attrs={'multiple': True}),
+        required=False
+    )
+
+class Form_Usu_Regis(UserCreationForm):
+    email = forms.EmailField(
+        label="",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese un Email'})
+    )
+    first_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Primer Nombre'})
+    )
+    last_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Nombre de Usuario'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Requerido. 150 caracteres o menos. letras, dígitos y @/./+/-/_ unicamente.</small></span>'
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Ingrese Contraseña'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = (
+            '<ul class="form-text text-muted small">'
+            '<li>Tu contraseña no debe ser similar a su información personal anterior.</li>'
+            '<li>Tu contraseña debe contener al menos 8 caracteres.</li>'
+            '<li>Tu contraseña no debe ser una facilmente hackeable.</li>'
+            '<li>Tu contraseña no puede ser enteramente numérica.</li>'
+            '</ul>'
+        )
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Ingrese Contraseña'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Ingresa la misma contraseña anterior. Por verificación.</small></span>'
+
+
+
+class Edit_Usu(UserChangeForm):
+    password = None #ocultar password
+    email = forms.EmailField(
+        label="",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese un Email'})
+    )
+    first_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Primer Nombre'})
+    )
+    last_name = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Nombre de Usuario'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Requerido. 150 caracteres o menos. letras, dígitos y @/./+/-/_ unicamente.</small></span>'
+
+class Usu_Info_Form(forms.ModelForm):
+    tfno = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Fono'}))
+    pais = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'País'}))
+    region = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Región'}))
+    ciu = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ciudad'}))
+    
+
+
+    class Meta:
+        model = Perfil
+        fields = ("tfno", "pais", "region", "ciu")
+
+    def clean_fecha_nac(self):
+        fecha_nac = self.cleaned_data.get('fecha_nac')
+        if not fecha_nac:
+            raise forms.ValidationError('Este campo es obligatorio.')
+        return fecha_nac
+
+
+class Edit_Cont(SetPasswordForm):
+    class Meta:
+        model = User
+        fields = ['new_password1', 'new_password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['new_password1'].widget.attrs['class'] = 'form-control'
+        self.fields['new_password1'].widget.attrs['placeholder'] = 'Ingrese Contraseña'
+        self.fields['new_password1'].label = ''
+        self.fields['new_password1'].help_text = (
+            '<ul class="form-text text-muted small">'
+            '<li>Tu contraseña no debe ser similar a su información personal anterior.</li>'
+            '<li>Tu contraseña debe contener al menos 8 caracteres.</li>'
+            '<li>Tu contraseña no debe ser una facilmente hackeable.</li>'
+            '<li>Tu contraseña no puede ser enteramente numérica.</li>'
+            '</ul>'
+        )
+
+        self.fields['new_password2'].widget.attrs['class'] = 'form-control'
+        self.fields['new_password2'].widget.attrs['placeholder'] = 'Ingrese Contraseña'
+        self.fields['new_password2'].label = ''
+        self.fields['new_password2'].help_text = '<span class="form-text text-muted"><small>Ingresa la misma contraseña anterior. Por verificación.</small></span>'
+
+        
---
+++ base_app/views.py
@@ -0,0 +1,134 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.db import transaction
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from base_app.models import Producto, Categoria, Marca, Condicion_Producto, Perfil, Foto
+from base_app.forms import PublicarProductoForm
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+
+def index(request):
+    productos_recientes = Producto.objects.all()[:6]  # Para el Carrousel, 6  productos más recientes
+    productos_descuento = Producto.objects.filter(descuento__isnull=False)[:8]  # Ejemplo: productos con descuento
+
+    context = {
+        'productos_recientes_start': productos_recientes[:3],
+        'productos_recientes_end': productos_recientes[3:],
+        'productos_descuento_start': productos_descuento[4:],
+        'productos_descuento_end': productos_descuento[:4],  
+    }
+    return render(request, 'index.html', context)
+
+def productos(request):
+    productos = Producto.objects.all()
+    context = {
+        'productos': productos,
+    }
+    return render(request, 'productos.html', context)
+
+def productos_filtrados(request, categoria):
+    categoriaProducto = Categoria.objects.get(nom_cate=categoria)
+    productos = Producto.objects.filter(id_cate=categoriaProducto).all()
+    context = {
+        'productos': productos,
+    }
+    return render(request, 'productos.html', context)
+
+def detalle_producto(request, id):
+    producto = Producto.objects.get(id_prod=id)
+    context = {
+        'producto': producto,
+    }
+    return render(request, 'detalle_prod.html', context)
+
+@login_required
+def publicar(request):
+    if request.method == 'POST':
+        form = PublicarProductoForm(request.POST, request.FILES)
+        if form.is_valid():
+            try:
+                with transaction.atomic():
+                    # Validar las relaciones
+                    marca = get_object_or_404(Marca, id_mar=request.POST.get('id_mar'))
+                    categoria = get_object_or_404(Categoria, id_cate=request.POST.get('id_cate'))
+                    condicion = get_object_or_404(Condicion_Producto, id_cond=request.POST.get('id_cond'))
+
+                    # Crear el producto
+                    producto = Producto.objects.create(
+                        nombre=form.cleaned_data['nombre'],
+                        descripcion=form.cleaned_data['descripcion'],
+                        precio=form.cleaned_data['precio'],
+                        descuento=form.cleaned_data['descuento'],
+                        id_mar=marca,
+                        id_cate=categoria,
+                        id_cond=condicion,
+                    )
+
+                    # Procesar las imágenes subidas
+                    imagenes = request.FILES.getlist('imagenes')
+                    for imagen in imagenes:
+                        # Verificar si el archivo es una imagen válida
+                        if imagen.content_type.startswith('image/'):
+                            fs = FileSystemStorage()
+                            filename = fs.save(imagen.name, imagen)
+                            ruta = fs.url(filename)
+                            Foto.objects.create(id_prod=producto, ruta_foto=ruta)
+                        else:
+                            raise ValueError("El archivo subido no es una imagen válida.")
+
+                    messages.success(request, "Producto publicado exitosamente.")
+                    return redirect('productos')
+            except Exception as e:
+                messages.error(request, f"Error al publicar el producto: {e}")
+        else:
+            messages.error(request, "Por favor, revisa los datos ingresados.")
+    else:
+        form = PublicarProductoForm()
+    return render(request, 'publicar.html', {'form': form})
+def login_required_message(request):
+    messages.error(request, "Debes iniciar sesión para publicar productos.")
+    return redirect('/login')
+
+def perfil_cliente(request):
+    return render(request, 'perfil/perfil_cliente.html')
+
+def perfil_usuario(request):
+    if request.user.is_authenticated:
+        user = request.user  # Usuario actual
+        try:
+            perfil = Perfil.objects.get(user=user)  # Obtenemos el perfil asociado al usuario
+        except Perfil.DoesNotExist:
+            perfil = None  # Si no existe el perfil, manejamos el caso adecuadamente
+
+        productos = Producto.objects.all()
+        context = {
+            'user': user,           # Pasamos los datos del usuario
+            'perfil': perfil,       # Pasamos el perfil del usuario
+            'productos': productos, # Pasamos los productos
+        }
+        return render(request, 'perfil/perfil_usuario.html', context)
+    else:
+        messages.error(request, "Debes iniciar sesión para ver tu perfil.")
+        return redirect('/login')
+
+
+
+def x(request):
+    return render (request, 'perfil/x.html')
+
+
+def terminos_condiciones(request):
+    return render(request, 'terminos_condiciones.html')
+
+def politicas_privacidad(request):
+    return render(request, 'politicas_privacidad.html')
+
+def preguntas_frecuentes(request):
+    return render(request, 'preguntas_frecuentes.html')
+
+def contacto(request):
+    """
+    Vista para renderizar el formulario de contacto.
+    """
+    return render(request, 'formulario_contacto.html')
